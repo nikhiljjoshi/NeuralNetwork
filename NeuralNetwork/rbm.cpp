50fdbe99f06d00c8b5393183ff15a534f58f90af
Nikhil J Joshi <nikhil.joshi@teamaol.com>
2015-05-06 13:41:37 -0700

    restricted boltzmann machine introduced

---
+++ NeuralNetwork/rbm.cpp
@@ -0,0 +1,9 @@
+//
+//  rbm.cpp
+//  NeuralNetwork
+//
+//  Created by Joshi, Nikhil on 5/6/15.
+//  Copyright (c) 2015 Nikhil Joshi. All rights reserved.
+//
+
+#include "rbm.hpp"
---
+++ NeuralNetwork/rbm.hpp
@@ -0,0 +1,14 @@
+//
+//  rbm.hpp
+//  NeuralNetwork
+//
+//  Created by Joshi, Nikhil on 5/6/15.
+//  Copyright (c) 2015 Nikhil Joshi. All rights reserved.
+//
+
+#ifndef __NeuralNetwork__rbm__
+#define __NeuralNetwork__rbm__
+
+#include <iostream>
+
+#endif /* defined(__NeuralNetwork__rbm__) */
